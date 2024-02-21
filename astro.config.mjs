57418e193d0ed0ecfe3563fa5cdfc7dceff873bf
nescpro <salazarnestor1996@gmail.com>
2024-02-21 13:58:51 -0500

    Build my first Astro island with Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://enchanting-pudding-775d70.netlify.app/"
+  site: "https://enchanting-pudding-775d70.netlify.app/",
+  integrations: [preact()]
 });
